3329c7131f5e65d48b03e77c34e362aec3ac7373
Patrizio Gasperi <patrizio.gasperi@gmail.com>
2021-03-31 07:47:22 +0200

    First commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>product List</h1>
+    <ul>
+      <li v-for="product in products">{{product.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import shop from '@/api/shop'
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  created() {
+    shop.getProducts(products => {
+      this.products = products
+    })
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
